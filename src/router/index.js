4bc811a2aed465dd312f78b68cfbcde16ed00582
HongJinWoo <ghdwlsdn0714@naver.com>
2020-06-19 10:01:40 +0900

    middle

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,31 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil.vue";
-import Hawaii from "../views/Hawaii.vue";
-import Panama from "../views/Panama.vue";
-import Jamaica from "../views/Jamaica.vue";
 
-Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
-    path: "/brazil",
-    name: "brazil",
-    component: Brazil
-  },
-  {
-    path: "/hawaii",
-    name: "hawaii",
-    component: Hawaii
-  },
-  {
-    path: "/panama",
-    name: "panama",
-    component: Panama
-  },
-  {
-    path: "/jamaica",
-    name: "jamaica",
-    component: Jamaica
-  }
-];
-
-const router = new VueRouter({
-  routes
+Vue.use(Router);
+export default new Router({
+  mode: "history",
+  linkExactActiveClass: "vue-school-active-class",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+      props: true
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue")
+    },
+    {
+      path:"/details/:slug",
+      name:"DestinationDetails",
+      props: true,
+      component:()=>import(/*webpackChunkname: "DestinationDetails"*/ "../views/DestinationDetails")
+      
+    }
+  ]
 });
-
-export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,67 @@
 <template>
   <div class="home">
     <h1>
-      All destinations
+      All Destinations
     </h1>
     <div class="destinations">
-    <div v-for="destinations in destinations"
-    :key="destinations.name">
-    <router-link :to="destinations.slug">
-      <h2>{{destinations.name}}</h2>
-    </router-link>
-    <figure>
-      <router-link :to="destinations.name">
-        <img :src="require(`@/assets/${destinations.image}`)" 
-        :alt="destinations.name">
-      </router-link>
-    </figure>
+      <div v-for="destination in destinations" :key="destination.name">
+        <router-link
+          :to="{
+            name: 'DestinationDetails',
+            params: { slug: destination.slug }
+          }"
+        >
+          <h2>{{ destination.name }}</h2>
+        </router-link>
+        <figure>
+          <router-link
+            :to="{
+              name: 'DestinationDetails',
+              params: { slug: destination.slug }
+            }"
+          >
+            <img
+              :src="require(`@/assets/${destination.image}`)"
+              :alt="destination.name"
+            />
+          </router-link>
+        </figure>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import store from "@/store.js"
+import store from "@/store.js";
 export default {
-  name: "Home",
+  name: "home",
   components: {},
   data() {
-    return{
+    return {
       destinations: store.destinations
-    }
+    };
   }
 };
 </script>
 <style scoped>
-.home{
-  max-width:1400px;
+.home {
+  max-width: 1400px;
   margin: 0 auto;
 }
-img{
+img {
   max-width: 200px;
 }
-.destinatios{
+.destinations {
   display: flex;
   justify-content: space-between;
+}
+a {
+  color: lightseagreen;
+  text-decoration: none;
+}
+a:hover,
+a:visited {
+  text-decoration: underline;
 }
 </style>
